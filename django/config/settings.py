c937b0bbc185e179865666ecb9332718b3509b6d
Alex <fastik17@gmail.com>
2019-05-07 19:34:52 +0300

    s

--- django/config/settings.py
+++ django/config/settings.py
@@ -146,7 +146,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
-    '/answerly/django/static/',
 ]
 MARKDOWNIFY_STRIP = False
 MARKDOWNIFY_WHITELIST_TAGS = [
